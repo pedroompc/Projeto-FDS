11df2de2c4ed618d6c5d826be75c62ab21437908
Gabriel Assef Nova <gan2@cesar.school>
2025-04-27 16:58:47 -0300

    atualizando app Ourbank

--- OurBank/urls.py
+++ OurBank/urls.py
@@ -1,15 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
-from transacoes import views as transacoes_views
-from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
 urlpatterns = [
-    path('', transacoes_views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('', include('user.urls')),
     path('transacoes/', include('transacoes.urls')),
-    path('user/', include('user.urls')),
-
-    # Rotas de autenticação (caso não estejam em user.urls)
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('forum/', include('forum.urls')),
 ]
