1a626e8242f0dc4bb4ae13a6bfd63defda4d373b
Gabriel Assef Nova <gan2@cesar.school>
2025-04-27 17:01:47 -0300

    Atualizando app user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    cpf = models.CharField(max_length=14, unique=True)
+    data_nascimento = models.DateField()
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    tipo_conta = models.CharField(max_length=10, choices=[('PESSOAL', 'Pessoal'), ('EMPRESA', 'Empresa')], default='PESSOAL')
+    
+    def __str__(self):
+        return f'Perfil de {self.usuario.username}'
