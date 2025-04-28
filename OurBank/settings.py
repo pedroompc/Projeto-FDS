3a49b6087cb254db2872b1d6937eeda92340b39b
Gabriel Assef Nova <gan2@cesar.school>
2025-04-28 12:09:05 -0300

    Update settings.py

--- OurBank/settings.py
+++ OurBank/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_t6o_h8vlsl2_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '169.254.129.4'] # Mantenha localhost e IPs se necessário
+ALLOWED_HOSTS = ['ourbank-hmbycrezencfbqh8.brazilsouth-01.azurewebsites.net', 'localhost', '169.254.129.4']
 
 AZURE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME')
 if AZURE_HOSTNAME:
